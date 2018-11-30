f11a420c97fd9e49f7c90f06a27828832a116887
ryanboris <ryan.boris@gmail.com>
2018-11-30 11:13:55 -0500

    Adds reguser registration endpoint

--- config/routes.js
+++ config/routes.js
@@ -2,16 +2,32 @@ const axios = require('axios')
 
 const { authenticate } = require('./middlewares')
 
+const db = require('../database/dbConfig.js')
+
+const bcrypt = require('bcryptjs')
+
 module.exports = server => {
   server.post('/api/register', register)
   server.post('/api/login', login)
   server.get('/api/jokes', authenticate, getJokes)
 }
 
-function register(req, res){
-  // implement user registration
-}
+async function register(req, res){
+  if (!req.body.username || !req.body.password) {
+    res.status(401).json({ message: 'Please provide a username and a password to register.' })
+  }
+  const creds = req.body
+  const hash = bcrypt.hashSync(creds.password, 14)
+  creds.password = hash
 
+  try {
+    const id = await db('users').insert(creds)
+    res.status(201).json({ id, success: 'Success.  User registered.' })
+  } catch (e) {
+    console.log(e)
+    res.status(500).json({ error: 'An error has occuried while trying to register with the database.' })
+  }
+}
 function login(req, res){
   // implement user login
 }
