9113a4b658768ae6dde71234c55419cba1e529fb
ryanboris <ryan.boris@gmail.com>
2018-11-30 11:08:20 -0500

    Install deps and change get req api

--- config/routes.js
+++ config/routes.js
@@ -1,30 +1,28 @@
-const axios = require('axios');
+const axios = require('axios')
 
-const { authenticate } = require('./middlewares');
+const { authenticate } = require('./middlewares')
 
 module.exports = server => {
-  server.post('/api/register', register);
-  server.post('/api/login', login);
-  server.get('/api/jokes', authenticate, getJokes);
-};
+  server.post('/api/register', register)
+  server.post('/api/login', login)
+  server.get('/api/jokes', authenticate, getJokes)
+}
 
-function register(req, res) {
+function register(req, res){
   // implement user registration
 }
 
-function login(req, res) {
+function login(req, res){
   // implement user login
 }
 
-function getJokes(req, res) {
+function getJokes(req, res){
   axios
-    .get(
-      'https://08ad1pao69.execute-api.us-east-1.amazonaws.com/dev/random_ten'
-    )
+    .get('https://safe-falls-22549.herokuapp.com/random_ten')
     .then(response => {
-      res.status(200).json(response.data);
+      res.status(200).json(response.data)
     })
     .catch(err => {
-      res.status(500).json({ message: 'Error Fetching Jokes', error: err });
-    });
+      res.status(500).json({ message: 'Error Fetching Jokes', error: err })
+    })
 }
